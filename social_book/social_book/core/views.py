544752ed1035ce5c8289dc1762a66442237974ce
Iconic Robbott <dev.robbott@gmail.com>
2025-04-06 01:34:11 +0100

    Modified follow/unfollow button for specific situations

--- social_book/social_book/core/views.py
+++ social_book/social_book/core/views.py
@@ -57,11 +57,22 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+        user_followers = FollowersCount.objects.filter(user=pk)
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
+        'button_text': button_text,
     }
     return render(request, 'profile.html', context)
 
